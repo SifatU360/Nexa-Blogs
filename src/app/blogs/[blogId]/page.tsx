820935e1a49a242981210249ee8e853364efc6fd
Sifat Ullah <158477820+SifatU360@users.noreply.github.com>
2025-12-19 10:25:46 +0600

    Generating Dynamic Metadata with generateMetadata()

--- src/app/blogs/[blogId]/page.tsx
+++ src/app/blogs/[blogId]/page.tsx
@@ -8,6 +8,19 @@ export const generateStaticParams = async() => {
     return blogs.slice(0,3).map((blog : Blog) => ({blogId : blog.id}));
 }
 
+export const generateMetadata = async({params} : {params: Promise<{blogId: string}>}) => {
+    const {blogId} = await params;
+    const res = await fetch(`http://localhost:5000/blogs/${blogId}`,{
+        cache: 'no-store',
+    });
+    const blog =  await res.json();
+
+    return {
+        title: blog.title,
+        description: blog.description,
+    }
+}
+
 const BlogDetailsPage = async({params} : {params: Promise<{blogId: string}>}) => {
     const {blogId} = await params;
     const res = await fetch(`http://localhost:5000/blogs/${blogId}`,{
